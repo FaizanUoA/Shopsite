aaf628af011cdf16185bb9962445a4d9a3e4ef2f
FaizanUoA <t17mk25@abdn.ac.uk>
2026-04-07 22:32:24 +0100

    updated items_list.html

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,10 @@
+from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, redirect, render
+from django.utils.http import url_has_allowed_host_and_scheme
 from django.views.decorators.http import require_POST
 
 from .models import Favourite, Item
@@ -48,6 +50,7 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, f"Welcome, {user.username}! Your account has been created.")
             return redirect("item_list")
     else:
         form = UserCreationForm()
@@ -69,5 +72,10 @@ def toggle_favourite(request, item_id):
     fav, created = Favourite.objects.get_or_create(user=request.user, item=item)
     if not created:
         fav.delete()
-    next_url = request.POST.get("next") or "/"
+        messages.success(request, f"“{item.name}” removed from your favourites.")
+    else:
+        messages.success(request, f"“{item.name}” added to your favourites.")
+    next_url = request.POST.get("next", "/")
+    if not url_has_allowed_host_and_scheme(next_url, allowed_hosts={request.get_host()}):
+        next_url = "/"
     return redirect(next_url)
